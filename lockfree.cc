a80ca42b97e8e9a8ba9f651c59625d5ded92d7c9
Artur Bac (linux) <ghaqurat@ebasoft.com.pl>
2019-03-12 01:04:06 +0100

    tests update

--- lockfree.cc
+++ lockfree.cc
@@ -9,16 +9,32 @@
 
 struct message_t
   { 
+  static int64_t instance_counter;
   uint32_t id;
   
-  message_t() noexcept : id{} {}
-  message_t(uint32_t pid ) noexcept : id{ pid } {}
+  message_t() noexcept : id{} 
+    { lockfree::atomic_add_fetch(&instance_counter, int64_t(1), lockfree::memorder::relaxed ); }
+    
+  message_t(uint32_t pid ) noexcept : id{ pid }
+    { lockfree::atomic_add_fetch(&instance_counter, int64_t(1), lockfree::memorder::relaxed ); }
+
+  message_t( message_t const & rh ) noexcept : id{ rh.id } 
+    { lockfree::atomic_add_fetch(&instance_counter, int64_t(1), lockfree::memorder::relaxed ); }
+
+  message_t & operator =( message_t const & rh ) noexcept
+    {
+    id = rh.id;
+    return *this;
+    }
+
+  ~message_t() 
+    { lockfree::atomic_sub_fetch(&instance_counter, int64_t(1), lockfree::memorder::relaxed ); }
   
   bool operator == ( message_t const & rh ) const noexcept { return id == rh.id; }
   bool operator != ( message_t const & rh ) const noexcept { return id != rh.id; }
   };
 
-  
+int64_t message_t::instance_counter = 0;
 
 using afifo_queue_type = lockfree::afifo_queue_t<message_t>;
 
@@ -30,6 +46,8 @@ static std::ostream & operator <<( std::ostream & stream, message_t sc )
   
 BOOST_AUTO_TEST_CASE( lock_free_afifo_test_single )
 {
+  message_t::instance_counter  = 0;
+  {
   afifo_queue_type queue;
   auto [it, succeed] { lockfree::queue_pull( queue ) };
   
@@ -62,10 +80,14 @@ BOOST_AUTO_TEST_CASE( lock_free_afifo_test_single )
     BOOST_TEST( succeed );
     BOOST_TEST( result == (message_t{i}) );
     }
+  }
+BOOST_TEST( message_t::instance_counter == 0 );
 }
 
 BOOST_AUTO_TEST_CASE( lock_free_afifo_test_2threads )
 {
+message_t::instance_counter  = 0;
+  {
   afifo_queue_type queue;
   uint64_t number_of_messages= 0x1FFFF;
   bool sender_finished {};
@@ -123,10 +145,14 @@ BOOST_AUTO_TEST_CASE( lock_free_afifo_test_2threads )
   sender.get();
   printf("sender finished\n");
   reciver.get();
+  }
+BOOST_TEST( message_t::instance_counter == 0 );
 }
 
 BOOST_AUTO_TEST_CASE( lock_free_afifo_test_multiple_threads )
 {
+message_t::instance_counter  = 0;
+  {
   afifo_queue_type queue;
   uint64_t number_of_messages= 0xFFFF;
   size_t number_of_senders = 16;
@@ -162,7 +188,7 @@ BOOST_AUTO_TEST_CASE( lock_free_afifo_test_multiple_threads )
                       else
                         lockfree::sleep(1);
                     }
-                    while( !sender_finished || !queue.empty() );
+                    while( !lockfree::atomic_load(sender_finished, lockfree::memorder::relaxed) || !queue.empty() );
                     
                     BOOST_TEST( (number_of_messages * number_of_senders) == last_message_nr );
                     
@@ -200,11 +226,15 @@ BOOST_AUTO_TEST_CASE( lock_free_afifo_test_multiple_threads )
   lockfree::atomic_add_fetch(&sender_finished,true, lockfree::memorder::relaxed );
   printf("sender finished\n");
   reciver.get();
+  }
+BOOST_TEST( message_t::instance_counter == 0 );
 }
 
 using lifo_type = lockfree::lifo_queue_t<message_t>;
 BOOST_AUTO_TEST_CASE( lock_free_lifo_test_single )
 {
+message_t::instance_counter  = 0;
+  {
   lifo_type queue;
   queue.push( message_t { 0 } );  
   
@@ -237,11 +267,16 @@ BOOST_AUTO_TEST_CASE( lock_free_lifo_test_single )
     }
   BOOST_TEST( number_of_messages == last_message_nr );
   BOOST_TEST( expected_sum == sum );
+  BOOST_TEST( queue.empty() );
   BOOST_TEST( queue.size() == 0 );
+  }
+BOOST_TEST( message_t::instance_counter == 0 );
 }
 
 BOOST_AUTO_TEST_CASE( lock_free_lifo_test_2threads )
 {
+message_t::instance_counter  = 0;
+  {
   lifo_type queue;
   uint64_t number_of_messages= 0x1FFFF;
   bool sender_finished {};
@@ -296,11 +331,15 @@ BOOST_AUTO_TEST_CASE( lock_free_lifo_test_2threads )
   sender.get();
   printf("sender finished\n");
   reciver.get();
-  
-
+  BOOST_TEST( queue.empty() );
+  BOOST_TEST( queue.size() == 0 );
+  }
+BOOST_TEST( message_t::instance_counter == 0 );
 }
 BOOST_AUTO_TEST_CASE( lock_free_lifo_test_multiple_threads )
 {
+message_t::instance_counter  = 0;
+  {
   lifo_type queue;
   uint64_t number_of_messages= 0x1FFFF;
   bool sender_finished {};
@@ -333,7 +372,8 @@ BOOST_AUTO_TEST_CASE( lock_free_lifo_test_multiple_threads )
                              else
                                lockfree::sleep(1);
                             }
-                            while( !sender_finished || !queue.empty() );
+                            while( !lockfree::atomic_load(sender_finished, lockfree::memorder::relaxed) || !queue.empty() );
+                            
                            BOOST_TEST( (number_of_messages * number_of_senders) == last_message_nr );
                            BOOST_TEST( expected_sum == sum );
                            });
@@ -365,14 +405,20 @@ BOOST_AUTO_TEST_CASE( lock_free_lifo_test_multiple_threads )
   
   for( auto & sender : senders )
     sender.get();
-  
+  printf("Senders send all data\n");
   lockfree::atomic_add_fetch(&sender_finished,true, lockfree::memorder::relaxed );
   reciver.get();
+  BOOST_TEST( queue.empty() );
+  BOOST_TEST( queue.size() == 0 );
+  }
+BOOST_TEST( message_t::instance_counter == 0 );
 }
 
 using fifo_type = lockfree::fifo_queue_t<message_t>;
 BOOST_AUTO_TEST_CASE( lock_free_fifo_test_single )
 {
+message_t::instance_counter  = 0;
+  {
   fifo_type queue;
   queue.push( message_t { 0 } );  
   
@@ -400,10 +446,16 @@ BOOST_AUTO_TEST_CASE( lock_free_fifo_test_single )
   std::tie(result,succeed) = lockfree::queue_pull( queue );
   BOOST_TEST( succeed );
   BOOST_TEST( result == (message_t{4}) );
+  BOOST_TEST( queue.empty() );
+  BOOST_TEST( queue.size() == 0 );
+  }
+BOOST_TEST( message_t::instance_counter == 0 );
 }
 
 BOOST_AUTO_TEST_CASE( lock_free_fifo_test_2threads )
 {
+message_t::instance_counter  = 0;
+  {
   fifo_type queue;
   uint32_t number_of_messages= 0x1FFFFF;
   auto reciver = std::async(std::launch::async,
@@ -429,11 +481,14 @@ BOOST_AUTO_TEST_CASE( lock_free_fifo_test_2threads )
                             });
   reciver.get();
   sender.get();
-
+  }
+BOOST_TEST( message_t::instance_counter == 0 );
 }
 
 BOOST_AUTO_TEST_CASE( lock_free_fifo_test_3threads_2recv_1send, * boost::unit_test::timeout(60) )
 {
+message_t::instance_counter  = 0;
+  {
   fifo_type queue;
   uint32_t number_of_messages= 0xFFFFF;
   uint32_t number_of_messages2= 0xAFFFF;
@@ -486,6 +541,9 @@ BOOST_AUTO_TEST_CASE( lock_free_fifo_test_3threads_2recv_1send, * boost::unit_te
   uint32_t sum = reciver.get() + reciver2.get();
   printf("recivers finished\n");
   BOOST_TEST( sum == (number_of_messages+number_of_messages2) );
-  
+  BOOST_TEST( queue.empty() );
+  BOOST_TEST( queue.size() == 0 );
+  }
+BOOST_TEST( message_t::instance_counter == 0 );
 }
 
